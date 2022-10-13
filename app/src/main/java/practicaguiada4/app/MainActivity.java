513a33462f1502d0914eb1144056eaf22a1c1d28
TardeDAM <jluisym16@educastur.es>
2022-10-13 19:22:46 +0200

    Initial commit

---
+++ app/src/main/java/practicaguiada4/app/MainActivity.java
@@ -0,0 +1,15 @@
+package practicaguiada4.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
