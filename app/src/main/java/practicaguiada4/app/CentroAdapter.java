f0608d1743fec591af874db2df5a13bd4e44b0bc
jluisym16 <jluisym16@educastur.es>
2022-10-20 11:53:11 +0200

    ..

--- app/src/main/java/practicaguiada4/app/CentroAdapter.java
+++ app/src/main/java/practicaguiada4/app/CentroAdapter.java
@@ -1,4 +1,29 @@
 package practicaguiada4.app;
 
-public class CentroAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import java.util.List;
+
+public class CentroAdapter extends ArrayAdapter<Centro> {
+    public CentroAdapter(@NonNull Context context, int resource, @NonNull List<Centro> objects) {
+        super(context, resource, objects);
+    }
+    
+    public View getView(int position, View convertView, ViewGroup parent){
+        Centro c = getItem(position);
+        if(convertView == null)
+            convertView= LayoutInflater.from(this.getContext()).inflate(R.layout.item, parent,false);
+        ((ImageView) convertView.findViewById((R.id.imageView)).setImageResource(c.getImagen()));
+        ((TextView) convertView.findViewById((R.id.nombre)).setText(c.getNombre()));
+        ((TextView) convertView.findViewById((R.id.direccion)).setText(c.getDireccion()));
+        return convertView;
+    }
 }
--- app/src/main/java/practicaguiada4/app/MainActivity.java
+++ app/src/main/java/practicaguiada4/app/MainActivity.java
@@ -4,12 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
-public class MainActivity extends ListActivity {
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends ListActivity implements AdapterView.OnItemClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<Centro> l = Arrays.asList(
+                new Centro ("IES Doctor Fleming", "Doctor Fleming 7, Oviedo", R.mipmap.ic_launcher),
+                new Centro ("IES Monte Naranco", "Calle de Oviedo", R.mipmap.ic_launcher),
+                new Centro ("CIFP Avilés", "Calle en Avilés", R.mipmap.ic_launcher)
+        );
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Centro centro= (Centro) adapterView.getItemAtPosition(i);
+        Toast.makeText(this, centro.getNombre(),Toast.LENGTH_SHORT).show();
     }
 }
